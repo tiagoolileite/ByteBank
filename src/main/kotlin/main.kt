92ad51e37d617f420016a5f281cbc3b508d2faff
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2021-11-11 23:10:35 -0300

    Aula 10 a 13

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,17 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
     //var ou val para variávies - val não pode ser alterado
-    val titular = "Tiago"
+    /*No caso do uso de var, sempre inicializar antes de atribuir novo valor ou utilizar a variável.
+    No caso de val, necessário inicialização*/
+    //Para declarar, usar : e o tipo. Ex: val titular: String = "Tiago Leite"
+    val titular = "Tiago Leite"
+    val numeroConta = 1000
+    //Double declarado implicitamente deve-se usar 0 . para informar que é um decimal
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
     //String template usando "$" e a variável desejada pode substituir a concatenação ("" + "")
-    print("Titular: $titular")
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo da conta: $saldo")
 }
