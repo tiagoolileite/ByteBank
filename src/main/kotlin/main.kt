6bc7f4df0cbb504fa7276942809a6110c5ccbe4d
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2021-11-13 21:09:26 -0300

    Aula 1 a 3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,64 +1,93 @@
 fun main() {
-
     println("Bem vindo ao Bytebank!")
 
-    var i = 0
-    while(i < 5) {
-        val titular = "Tiago Leite $i"
-        val numeroConta = 1000 + i
-        var saldo = 10.0
-        saldo += i
+    val contaTiago = Conta()
+    contaTiago.titular = "Tiago"
+    contaTiago.numero = 1000
+    contaTiago.saldo = 200.0
+    println(contaTiago.titular)
+    println(contaTiago.numero)
+    println(contaTiago.saldo)
 
-        println("Titular: $titular")
-        println("Número da conta: $numeroConta")
-        println("Saldo da conta: $saldo")
-
-        testaCondicao(saldo, numeroConta)
-        println()
-        i++
-    }
-
-    /*for (i in 1..5) { //para decrescente, usar downTo (i in 5 downTo 1)
-                      //Usar step para configurar a forma de incrementação (i in 5 downTo 1 step 2) - pula de 2 em 2
-        if (i == 4) {
-            continue // continue no laço de repetição força o próximo passo. break força a finalização do loop
-        }
-        //var ou val para variávies - val não pode ser alterado
-        *//*No caso do uso de var, sempre inicializar antes de atribuir novo valor ou utilizar a variável.
-        No caso de val, necessário inicialização*//*
-        //Para declarar, usar : e o tipo. Ex: val titular: String = "Tiago Leite"
-        val titular = "Tiago Leite $i"
-        val numeroConta = 1000 + i
-        //Double declarado implicitamente deve-se usar 0 . para informar que é um decimal
-        var saldo = 10.0
-        saldo += i
-
-        //String template usando "$" e a variável desejada pode substituir a concatenação ("" + "")
-        println("Titular: $titular")
-        println("Número da conta: $numeroConta")
-        println("Saldo da conta: $saldo")
-
-        *//*for (i in 1..3) {
-            println(i)
-        }*//*
-
-        *//*when {
-        saldo > 0.0 -> println("A conta $numeroConta é positiva. Saldo: $saldo")
-        saldo == 0.0 -> println("A conta $numeroConta é neutra. (saldo zerado)")
-        else -> println("A conta $numeroConta é negativa. Saldo: $saldo")
-        }*//*
-
-        testaCondicao(saldo, numeroConta)
-        println()
-    }*/
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
 }
 
-fun testaCondicao(saldo: Double, numeroConta: Int) { //Passando parâmetro em Kotlin, necessário declarar o tipo
-    if (saldo > 0.0) { //Para linha única na instrução de execução, podem ser ocultadas as chaves "{ }"
-        println("A conta $numeroConta é positiva. Saldo: $saldo")
-    } else if (saldo == 0.0) {
-        println("A conta $numeroConta é neutra. (saldo zerado)")
-    } else {
-        println("A conta $numeroConta é negativa. Saldo: $saldo")
-    }
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
 }
+
+// fun testaLacos() {
+//
+// var i = 0
+// while(i < 5) {
+// val titular = "Tiago Leite $i"
+// val numeroConta = 1000 + i
+// var saldo = 10.0
+// saldo += i
+//
+// println("Titular: $titular")
+// println("Número da conta: $numeroConta")
+// println("Saldo da conta: $saldo")
+//
+// testaCondicao(saldo, numeroConta)
+// println()
+// i++
+//
+// }
+//
+// @Suppress("NAME_SHADOWING")
+// for (i in 1..5) { //para decrescente, usar downTo (i in 5 downTo 1)
+//
+// //Usar step para configurar a forma de incrementação (i in 5 downTo 1 step 2) - pula de 2 em 2
+// if (i == 4) {
+// continue // continue no laço de repetição força o próximo passo. break força a finalização do loop
+// }
+// //var ou val para variávies - val não pode ser alterado
+// /*No caso do uso de var, sempre inicializar antes de atribuir novo valor ou utilizar a variável.
+// No caso de val, necessário inicialização*/
+// //Para declarar, usar : e o tipo. Ex: val titular: String = "Tiago Leite"
+// val titular = "Tiago Leite $i"
+// val numeroConta = 1000 + i
+// //Double declarado implicitamente deve-se usar 0 . para informar que é um decimal
+// var saldo = 10.0
+// saldo += i
+//
+// //String template usando "$" e a variável desejada pode substituir a concatenação ("" + "")
+// println("Titular: $titular")
+// println("Número da conta: $numeroConta")
+// println("Saldo da conta: $saldo")
+//
+// /*for (i in 1..3) {
+// println(i)
+// }*/
+//
+// testaCondicao(saldo, numeroConta)
+// println()
+//
+// }
+// }
+//
+// fun testaCondicao(saldo: Double, numeroConta: Int) { //Passando parâmetro em Kotlin, necessário declarar o tipo
+//
+// when {
+// saldo > 0.0 -> println("A conta $numeroConta é positiva. Saldo: $saldo")
+// saldo == 0.0 -> println("A conta $numeroConta é neutra. (saldo zerado)")
+// else -> println("A conta $numeroConta é negativa. Saldo: $saldo")
+// }
+//
+// /*if (saldo > 0.0) { //Para linha única na instrução de execução, podem ser ocultadas as chaves "{ }"
+// println("A conta $numeroConta é positiva. Saldo: $saldo")
+// } else if (saldo == 0.0) {
+// println("A conta $numeroConta é neutra. (saldo zerado)")
+// } else {
+// println("A conta $numeroConta é negativa. Saldo: $saldo")
+// }*/
+// }
