bb102d0f5d9f99cc5bb13c7bc00566f7a66449a3
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2021-11-14 00:36:51 -0300

    Aula 6 e 7 - Criando uma nova classe
    Criação da classe funcionário e organização de código.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,14 @@
 fun main() {
     println("Bem vindo ao Bytebank!\n")
 
-}
+    val tiago = Funcionario(
+        nome = "Tiago",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
 
+    println("Nome: ${tiago.nome}")
+    println("CPF: ${tiago.cpf}")
+    println("Salário: R$${tiago.salario}")
+    println("Bonificação: R$${tiago.bonificacao()}")
+}
