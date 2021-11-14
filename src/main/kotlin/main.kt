18958456048c008244a44b056128f3cf3b859eff
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2021-11-14 11:55:28 -0300

    Aula 1 a 2 - Herdando de funcionario
    Implementando herança de funcionarios para os demais tipos (Gerente e Diretor)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,7 +14,7 @@ fun main() {
 
     val fran = Gerente(
         nome = "Fran",
-        cpf = "111.111.111-11",
+        cpf = "111.111.111-12",
         salario = 2000.0,
         senha = 1234
     )
@@ -25,6 +25,26 @@ fun main() {
     println("Bonificação: R$${fran.bonificacao()}\n")
 
     if(fran.autentica(1234)) {
+        println("Autenticação realizada com sucesso\n")
+    } else {
+        println("Falha na autenticação\n")
+    }
+
+    val gui = Diretor(
+        nome = "Gui",
+        cpf = "111.111.111-13",
+        salario = 3000.0,
+        senha = 4000,
+        plr = 200.0
+    )
+
+    println("Nome: ${gui.nome}")
+    println("CPF: ${gui.cpf}")
+    println("Salário: R$${gui.salario}")
+    println("Bonificação: R$${gui.bonificacao()}")
+    println("PLR: R$${gui.plr}\n")
+
+    if(gui.autentica(4000)) {
         println("Autenticação realizada com sucesso\n")
     } else {
         println("Falha na autenticação\n")
