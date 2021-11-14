8607d12c3eb068d5cc3177253cca28c1da1306c8
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2021-11-14 13:44:17 -0300

    Aula 1 e 2 - Utilizando abstrações

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,4 @@
 fun main() {
     println("Bem vindo ao Bytebank!\n")
 
-    val contaCorrente = ContaCorrente(
-        titular = "Tiago Leite",
-        numero = 1000
-    )
-
-    val contaPoupanca = ContaPoupanca(
-        titular = "Fran",
-        numero = 1001
-    )
-
-    contaCorrente.deposita(1000.0)
-    contaPoupanca.deposita(1000.0)
-
-    contaCorrente.saca(100.0)
-    contaPoupanca.saca(100.0)
-
-    contaCorrente.transfere(100.0, contaPoupanca)
-    contaPoupanca.transfere(200.0, contaCorrente)
-
 }
---
+++ src/main/kotlin/testaContasDiferentes.kt
@@ -0,0 +1,20 @@
+fun testaContasDiferentes() {
+    val contaCorrente = ContaCorrente(
+        titular = "Tiago Leite",
+        numero = 1000
+    )
+
+    val contaPoupanca = ContaPoupanca(
+        titular = "Fran",
+        numero = 1001
+    )
+
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita(1000.0)
+
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
+
+    contaCorrente.transfere(100.0, contaPoupanca)
+    contaPoupanca.transfere(200.0, contaCorrente)
+}
