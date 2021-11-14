785b8d59eb0fc49d27b2fcbb60794e378c373d49
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2021-11-14 01:00:39 -0300

    Aula 11 a 13 - Criando classes para cada tipo de funcionário

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -2,18 +2,8 @@ class Funcionario(
     val nome: String,
     val cpf: String,
     val salario: Double,
-    val tipo: Int //0 - funcionario, 1 - gerente, 2 - diretor
 ) {
     fun bonificacao(): Double {
-        return when (tipo) {
-            0 -> salario * 0.1
-            1 -> salario * 0.2
-            2 -> salario * 0.3
-            else -> salario * 0.1
-        }
-    }
-
-    fun autentica(senha: Int) {
-        //TODO
+        return salario * 0.1
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,12 +4,30 @@ fun main() {
     val tiago = Funcionario(
         nome = "Tiago",
         cpf = "111.111.111-11",
-        salario = 1000.0,
-        tipo = 0
+        salario = 1000.0
     )
 
     println("Nome: ${tiago.nome}")
     println("CPF: ${tiago.cpf}")
     println("Salário: R$${tiago.salario}")
-    println("Bonificação: R$${tiago.bonificacao()}")
+    println("Bonificação: R$${tiago.bonificacao()}\n")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "111.111.111-11",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("Nome: ${fran.nome}")
+    println("CPF: ${fran.cpf}")
+    println("Salário: R$${fran.salario}")
+    println("Bonificação: R$${fran.bonificacao()}\n")
+
+    if(fran.autentica(1234)) {
+        println("Autenticação realizada com sucesso\n")
+    } else {
+        println("Falha na autenticação\n")
+    }
+
 }
